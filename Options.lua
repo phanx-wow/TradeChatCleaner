a689dbe0a98ade16a3186ca1c87cb40680c20ef0
Phanx <addons@phanx.net>
2014-12-29 23:58:40 -0800

    Better fix for editbox height
    Explicitly setting the height doesn't actually have an effect when it's a ScrollChild, so just use an additional overlay button to help with focusing on click

--- Options.lua
+++ Options.lua
@@ -13,24 +13,26 @@ local Options = CreateFrame("Frame", NAME.."Options", InterfaceOptionsFramePanel
 Options.name = NAME
 InterfaceOptions_AddCategory(Options)
 
-local Title = Options:CreateFontString("$parentTitle", "ARTWORK", "GameFontNormalLarge")
+Options:Hide()
+Options:SetScript("OnShow", function()
+
+local Title = Options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 Title:SetPoint("TOPLEFT", 16, -16)
 Title:SetText(NAME)
 Options.Title = Title
 
-local SubText = Options:CreateFontString("$parentSubText", "ARTWORK", "GameFontHighlightSmall")
+local SubText = Options:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 SubText:SetPoint("TOPLEFT", Title, "BOTTOMLEFT", 0, -8)
 SubText:SetPoint("RIGHT", -16, 0)
-SubText:SetHeight(64)
+SubText:SetHeight(40) -- TODO: check height with translations
 SubText:SetJustifyH("LEFT")
 SubText:SetJustifyV("TOP")
+--SubText:SetWordWrap(true)
+--SubText:SetIndentedWordWrap(false)
 SubText:SetText(L.Description)
 Options.SubText = SubText
 
 local function MakeMultiLineEditBox(name)
-	local label = Options:CreateFontString("$parent"..name.."Label", "ARTWORK", "GameFontNormal")
-	label:SetJustifyH("LEFT")
-
 	local bg = CreateFrame("Frame", nil, Options)
 	bg:SetBackdrop({
 		bgFile = [[Interface\Tooltips\UI-Tooltip-Background]],
@@ -40,16 +42,21 @@ local function MakeMultiLineEditBox(name)
 	bg:SetBackdropColor(0, 0, 0)
 	bg:SetBackdropBorderColor(0.4, 0.4, 0.4)
 
+	local label = bg:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+	label:SetPoint("BOTTOMLEFT", bg, "TOPLEFT", 4, 0)
+	label:SetPoint("BOTTOMRIGHT", bg, "TOPRIGHT", -4, 0)
+	label:SetJustifyH("LEFT")
+
 	local scroll = CreateFrame("ScrollFrame", "$parent"..name.."Scroll", bg, "UIPanelScrollFrameTemplate")
-	scroll:SetPoint("TOPLEFT", 8, -6)
-	scroll:SetPoint("BOTTOMRIGHT", -4, 6)
+	scroll:SetPoint("TOPLEFT", 9, -10) -- 8, -6)
+	scroll:SetPoint("BOTTOMRIGHT", -4, 8) -- -4, 6)
 
 	local bar = _G[scroll:GetName().."ScrollBar"]
 	bar:ClearAllPoints()
-	bar:SetPoint("TOPRIGHT", -1, -15)
+	bar:SetPoint("TOPRIGHT", -1, -12)
 	bar:SetPoint("BOTTOMRIGHT", -1, 15)
 
-	local edit = CreateFrame("EditBox", "$parent"..name, Options)
+	local edit = CreateFrame("EditBox", "$parent"..name, bg)
 	edit:SetPoint("BOTTOMLEFT", scroll)
 	edit:SetPoint("TOPRIGHT", scroll, -bar:GetWidth(), 0)
 	edit:EnableMouse(true)
@@ -59,7 +66,7 @@ local function MakeMultiLineEditBox(name)
 	edit:SetMultiLine(true)
 	edit:SetScript("OnTabPressed", edit.ClearFocus)
 	edit:SetScript("OnEscapePressed", edit.ClearFocus)
-	edit:SetScript("OnCursorChanged", function(this, x, y, _, cursorHeight)
+	edit:SetScript("OnCursorChanged", function(self, x, y, _, cursorHeight)
 		y = -y
 		local offset = scroll:GetVerticalScroll()
 		if y < offset then
@@ -72,28 +79,73 @@ local function MakeMultiLineEditBox(name)
 		end
 	end)
 
+	local focus = CreateFrame("Button", nil, scroll)
+	focus:SetPoint("BOTTOMLEFT", -2, -1)
+	focus:SetPoint("TOPRIGHT", 1 - bar:GetWidth(), 1)
+	--focus:SetBackdrop({ bgFile = "Interface\\BUTTONS\\WHITE8X8" })
+	--focus:SetBackdropColor(1, 1, 1, 0.25)
+	focus:SetScript("OnClick", function(self)
+		edit:SetFocus()
+	end)
+	edit:SetScript("OnEditFocusGained", function(self)
+		focus:Hide()
+	end)
+	edit:SetScript("OnEditFocusLost", function(self)
+		focus:Show() -- focus:SetShown(self:GetHeight() < scroll:GetHeight())
+	end)
+
+	local accept = CreateFrame("Button", "$parent"..name.."Button", scroll, "UIPanelButtonTemplate")
+	accept:SetPoint("TOP", bg, "BOTTOM", 0, -1)
+	accept:SetSize(180, 20)
+	accept:SetText(ACCEPT)
+
 	scroll:SetScrollChild(edit)
 	edit:SetFrameLevel(scroll:GetFrameLevel() + 1)
-
-	scroll:HookScript("OnSizeChanged", function(this, width, height)
+	scroll:HookScript("OnSizeChanged", function(self, width, height)
 		edit:SetWidth(width - bar:GetWidth())
 	end)
-	scroll:HookScript("OnVerticalScroll", function(this, offset)
-		edit:SetHitRectInsets(0, 0, offset, edit:GetHeight() - offset - this:GetHeight())
+	scroll:HookScript("OnVerticalScroll", function(self, offset)
+		edit:SetHitRectInsets(0, 0, offset, edit:GetHeight() - offset - self:GetHeight())
 	end)
 
-	local accept = CreateFrame("Button", "$parent"..name.."Accept", Options, "UIPanelButtonTemplate")
-	accept:SetPoint("TOP", bg, "BOTTOM", 0, -4)
-	accept:SetSize(120, 20)
-	accept:SetText(ACCEPT)
+	function edit:SetPoint(point, relativeTo, relativePoint, x, y)
+		print("SetPoint", tostringall(point, relativeTo, relativePoint, x, y))
+		if relativeTo == nil or type(relativeTo) == "number" then
+			-- "TOPLEFT", x, y
+			point, relativeTo, relativePoint, x, y = point, Options, point, relativeTo, relativePoint
+		elseif relativePoint == nil or type(relativePoint) == "number" then
+			-- "TOPLEFT", someFrame, x, y
+			point, relativeTo, relativePoint, x, y = point, relativeTo, point, relativePoint, x
+		end
+		if strmatch(point, "LEFT") then
+			x = (x or 0) - 4
+		elseif strmatch(point, "RIGHT") then
+			x = (x or 0) + 4
+		end
+		if strmatch(point, "TOP") then
+			y = (y or 0) - label:GetStringHeight()
+		elseif strmatch(point, "BOTTOM") then
+			y = (y or 0) + 1 + accept:GetHeight()
+		end
+		print("      =>", tostringall(point, relativeTo, relativePoint, x, y))
+		bg:SetPoint(point, relativeTo, relativePoint, x or 0, y or 0)
+	end
 
 	edit.bg = bg
 	edit.label = label
 	edit.scrollBar = bar
 	edit.scrollFrame = scroll
 	edit.acceptButton = accept
+	edit.focusClicker = focus
 
 	return edit
+end
+
+local function FillEditBoxFromList(editBox, list)
+	editBox:SetCursorPosition(0)
+	editBox:ClearFocus()
+	editBox:SetText(table.concat(list, "\n"))
+	editBox.scrollFrame:SetVerticalScroll(0)
 end
 
 local function FillListFromText(list, text)
@@ -119,69 +171,43 @@ local function FillListFromText(list, text)
 end
 
 local Blacklist = MakeMultiLineEditBox("Blacklist")
+Blacklist:SetPoint("TOPLEFT", SubText, "BOTTOMLEFT", 0, -16)
+Blacklist:SetPoint("TOPRIGHT", SubText, "BOTTOM", -8, -16)
+Blacklist:SetPoint("BOTTOMLEFT", 16, 16)
+Blacklist.label:SetText(L.Blacklist)
 Options.Blacklist = Blacklist
 
-Blacklist.label:SetPoint("TOPLEFT", SubText, "BOTTOMLEFT", 0, -16)
-Blacklist.label:SetPoint("TOPRIGHT", SubText, "BOTTOM", -8, -16)
-Blacklist.label:SetText(L.Blacklist)
-
-Blacklist.bg:SetPoint("TOPLEFT", Blacklist.label, "BOTTOMLEFT", -4, 0)
-Blacklist.bg:SetPoint("TOPRIGHT", Blacklist.label, "BOTTOMRIGHT", 4, 0)
-Blacklist.bg:SetPoint("BOTTOMLEFT", Options, 16, 16 + 4 + 20)
-
 local Whitelist = MakeMultiLineEditBox("Whitelist")
+Whitelist:SetPoint("TOPLEFT", SubText, "BOTTOM", 8, -16)
+Whitelist:SetPoint("TOPRIGHT", SubText, "BOTTOMRIGHT", 0, -16)
+Whitelist:SetPoint("BOTTOMRIGHT", -16, 16)
+Whitelist.label:SetText(L.Whitelist)
 Options.Whitelist = Whitelist
 
-Whitelist.label:SetPoint("TOPLEFT", SubText, "BOTTOM", 8, -16)
-Whitelist.label:SetPoint("TOPRIGHT", SubText, "BOTTOMRIGHT", 0, -16)
-Whitelist.label:SetText(L.Whitelist)
-
-Whitelist.bg:SetPoint("TOPLEFT", Whitelist.label, "BOTTOMLEFT", -4, 0)
-Whitelist.bg:SetPoint("TOPRIGHT", Whitelist.label, "BOTTOMRIGHT", 4, 0)
-Whitelist.bg:SetPoint("BOTTOMRIGHT", Options, -16, 16 + 4 + 20)
-
-local function RefreshEditBoxFromList(editBoxName, listName)
-	editBoxName:SetCursorPosition(0)
-	editBoxName:ClearFocus()
-	editBoxName:SetText(table.concat(listName, "\n"))
-	
-	if #listName > 0 then
-		local _, lineHeight = editBoxName:GetFontObject():GetFont()
-		editBoxName:SetHeight(lineHeight * #listName)
-	end
-	editBoxName.scrollFrame:SetVerticalScroll(0)
-end
-
-local function RefreshBlacklist()
-	RefreshEditBoxFromList(Blacklist, ChatCleanerBlacklist)
-end
-
-local function RefreshWhitelist()
-	RefreshEditBoxFromList(Whitelist, ChatCleanerWhitelist)
-end
-
-Blacklist.acceptButton:SetScript("OnClick", function(this)
+Blacklist.acceptButton:SetScript("OnClick", function(self)
 	FillListFromText(ChatCleanerBlacklist, Blacklist:GetText())
-	RefreshBlacklist()
+	Options:refresh()
 end)
 
-Whitelist.acceptButton:SetScript("OnClick", function(this)
+Whitelist.acceptButton:SetScript("OnClick", function(self)
 	FillListFromText(ChatCleanerWhitelist, Whitelist:GetText())
-	RefreshWhitelist()
+	Options:refresh()
 end)
 
 function Options:refresh()
 	table.sort(ChatCleanerBlacklist)
-	RefreshBlacklist()
+	FillEditBoxFromList(Blacklist, ChatCleanerBlacklist)
 
 	table.sort(ChatCleanerWhitelist)
-	RefreshWhitelist()
+	FillEditBoxFromList(Whitelist, ChatCleanerWhitelist)
 end
 
+Options:refresh()
 Options:SetScript("OnShow", Options.refresh)
+
+end) -- one-time OnShow
 
 SLASH_TRADECHATCLEANER1 = "/tcc"
 SlashCmdList.TRADECHATCLEANER = function()
-	InterfaceOptionsFrame_OpenToCategory(Options)
 	InterfaceOptionsFrame_OpenToCategory(Options)
 end
--- Strings.lua
+++ Strings.lua
@@ -12,7 +12,7 @@ local _, L = ...
 L.Trade = "Trade"
 L.Blacklist = "Blacklisted Words"
 L.Whitelist = "Whitelisted Words"
-L.Description = "Messages in Trade chat are blocked unless they contain at least one whitelisted word |cffffd200and|r do not contain any blacklisted words.\n\nThe blacklist (but not the whitelist) is also applied to General chat."
+L.Description = "Messages in Trade chat are blocked unless they contain at least one whitelisted word |cffffd200and|r do not contain any blacklisted words. The blacklist (but not the whitelist) is also applied to General chat."
 
 local LOC = GetLocale()
 if LOC == "deDE" then
@@ -20,14 +20,14 @@ if LOC == "deDE" then
 	L.Trade = "Handel"
 	L.Blacklist = "Verbotene Wörter"
 	L.Whitelist = "Zulässige Wörter"
-	L.Description = "Meldungen im Handeln-Channel werden nur angezeigt, wenn sie mindestens ein zulässiges Wort |cffffd200und|r keine verbotenen Wörter enthalten.\n\nDie Liste der verbotenen Wörter (aber nicht die der zulässigen) ist auch mit dem Allgemein-Channel angewendet."
+	L.Description = "Meldungen im Handeln-Channel werden nur angezeigt, wenn sie mindestens ein zulässiges Wort |cffffd200und|r keine verbotenen Wörter enthalten. Die Liste der verbotenen Wörter (aber nicht die der zulässigen) ist auch mit dem Allgemein-Channel angewendet."
 
 elseif LOC == "esES" or LOC == "esMX" then
 
 	L.Trade = "Comercio"
 	L.Blacklist = "Palabras prohibidas"
 	L.Whitelist = "Palabras permitidas"
-	L.Description = "Los mensajes en el canal Comercio se muestran solamente si contienen al menos una palabra permitida |cffffd200y|r también no contienen ningún palabras prohibidas.\n\nLa lista de palabras prohibidas (pero no las permitidas) se aplica también al canal General."
+	L.Description = "Los mensajes en el canal Comercio se muestran solamente si contienen al menos una palabra permitida |cffffd200y|r también no contienen ningún palabras prohibidas. La lista de palabras prohibidas (pero no las permitidas) se aplica también al canal General."
 
 elseif LOC == "frFR" then
 
