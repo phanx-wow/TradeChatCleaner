2862ea563f2564d6b72984a16ac6824b4bb0f725
Phanx <addons@phanx.net>
2015-01-05 08:39:41 -0800

    Fix returning player GUID, update keywords

--- Addon.lua
+++ Addon.lua
@@ -11,30 +11,37 @@ local _, L = ...
 
 ChatCleanerWhitelist = {
 	"|h", -- links
+	"%f[%a]rukh", -- rukh, rukhan
+	"blingtron",
 	"dps",
+	"drov",
 	"ffa",
 	"flex",
 	"free ?roll",
 	"he[ai]l",
 	"heroic",
-	"%f[%a]lf[gmw%d]%f[%A]", -- lfg, lfm, lfw
+	"%f[%a]lf[gmw%d]?%f[%A]", -- lfg, lfm, lfw, lf5, lf ___
 	"kaufe", -- de
 	"mog run",
 	"mythic",
 	"reserve",
 	"s[cz]ena?r?i?o?", -- en/de
 	"style ?ru[ns]h?", -- en/de
+	"tar[il]na", -- tarlna, but people are dumb and also write tarina
 	"transmog",
 	"%f[%a]vk%f[%A]", -- de
 	"tank",
 	"trash farm",
+	"weltboss",
 	"world ?boss",
-	"^wt[bst]",
+	"^wt[bst]", -- wtb, wts, wtt
 }
 
 ChatCleanerBlacklist = {
 	-- real spam
 	"%.c0m%f[%A]",
+	"%S+#%d+", -- BattleTag
+	"%d/%d cm gold",
 	"%d%s?eur%f[%A]",
 	"%d%s?usd%f[%A]",
 	"account",
@@ -47,10 +54,10 @@ ChatCleanerBlacklist = {
 	"name change",
 	"paypal",
 	"professional",
-	"qq",
+	"qq", -- Chinese IM network, also catches junk as a bonus!
 	"ranking",
 	"realm",
-	"selfplay",
+	"self ?play",
 	"server",
 	"share",
 	"s%A*k%A*y%A*p%Ae", -- spammers love to obfuscate "skype"
@@ -59,6 +66,7 @@ ChatCleanerBlacklist = {
 	-- pvp
 	"[235]v[235]",
 	"%f[%a]arena", -- arenacap, arenamate, arenapoints
+	"%f[%a]cap%f[%A]",
 	"%f[%a]carry%f[%A]",
 	"%f[%a]cr%f[%A]",
 	"%f[%d][235]s%f[%A]", -- 2s, 3s, 5s
@@ -76,6 +84,8 @@ ChatCleanerBlacklist = {
 	"season",
 	"weekly cap",
 	-- junk
+	"%[dirge%]",
+	"%f[%a]ebay",
 	"a?m[eu]rican?", -- america, american, murica
 	"an[au][ls]e?r?%f[%L]", -- anal, anus, -e/er/es/en
 	"argument",
@@ -83,11 +93,14 @@ ChatCleanerBlacklist = {
 	"australi",
 	"bacon",
 	"bewbs",
+	"bitch",
 	"boobs",
+	"christian",
 	"chuck ?norris",
 	"girl",
 	"kiss",
 	"mad ?bro",
+	"mudda",
 	"muslim",
 	"nigg[ae]r?",
 	"obama",
@@ -114,13 +127,13 @@ local reqLatin = not strmatch(GetLocale(), "^[rkz][uoh]")
 
 local strmatch, strlower, type = string.match, string.lower, type
 
-local prevID, result, prevText
-ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, message, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, ...)
+local prevID, result
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, message, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, senderGUID, ...)
 	if lineID == prevID then
 		if result == true then
-			return result
+			return true
 		else
-			return false, result, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, ...
+			return false, result, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, senderGUID, ...
 		end
 	end
 	prevID, result = lineID, true
@@ -128,7 +141,7 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, message, send
 	-- Don't filter custom channels
 	if channelID == 0 or type(channelID) ~= "number" then return end
 
-	local search = gsub(strlower(message), "\124h", strupper)
+	local search = strlower(message)
 
 	-- Hide ASCII art crap
 	if reqLatin and not strmatch(search, "[a-z]") then
@@ -139,7 +152,7 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, message, send
 	local blacklist = ChatCleanerBlacklist
 	for i = 1, #blacklist do
 		if strmatch(search, blacklist[i]) then
-			--print("Blacklisted:", channelID, blacklist[i])
+			--print("Blacklisted:", blacklist[i])
 			--print("  ", search)
 			return true
 		end
@@ -162,7 +175,7 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(_, _, message, send
 	if pass then
 		--print("Passed")
 		result = message
-		return false, message, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, ...
+		return false, message, sender, arg3, arg4, arg5, flag, channelID, arg8, channelName, arg10, lineID, senderGUID, ...
 	end
 
 	--print("Other:", channelID, search)
